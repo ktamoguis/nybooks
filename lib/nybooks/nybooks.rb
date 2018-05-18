33c8e847f2adf29dac25a2fcc94c66a81bbb1742
KTAmoguis <ktamoguis@gmail.com>
2018-05-18 16:51:29 +0800

    put files into lib/nybooks as stated in README

---
+++ lib/nybooks/nybooks.rb
@@ -0,0 +1,38 @@
+#require "nybooks/version"
+require_relative "../nybooks/command_line_interface.rb"
+require_relative "../nybooks/scraper.rb"
+require 'pry'
+#module Nybooks
+  # Your code goes here...
+#end
+
+class Books
+
+  attr_accessor :title, :author, :description, :freshness
+
+  @@all = []
+
+  def initialize(bookhash)
+    @title = bookhash[:title]
+    @author = bookhash[:author]
+    @description = bookhash[:description]
+    @freshness = bookhash[:freshness]
+    @@all << self
+  end
+
+  def self.create_from_collection(books_array)
+    #binding.pry
+    books_array.each do |book|
+      Books.new(book)
+    end
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+  def self.all
+    @@all
+  end
+
+end
---
+++ lib/nybooks/scraper.rb
@@ -0,0 +1,30 @@
+require_relative "../nybooks/command_line_interface.rb"
+
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+class Scraper
+
+  def self.scrape_page(url)
+    webpage = Nokogiri::HTML(open(url))
+    i = 0
+    books = []
+    doc = webpage.css("ol.book-menu div.book-body")
+    #binding.pry
+    while i < 14 do
+      doc.each do |book|
+        title = doc[i].css("h2.title").text
+        author = doc[i].css("p.author").text
+        description = doc[i].css("p.description").text
+        freshness = doc[i].css("p.freshness").text
+        books << {:title => title, :author => author, :description => description, :freshness => freshness}
+        i +=1
+      end
+    end
+    books
+    #binding.pry
+  end
+
+
+end
