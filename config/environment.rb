7cf6d609a23732759d06090944bc44044ba75d91
KTAmoguis <ktamoguis@gmail.com>
2018-05-18 22:58:48 +0800

    added config folder

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'nokogiri'
+require 'pry'
+require 'open-uri'
+
+require_relative "../lib/nybooks/command_line_interface.rb"
+require_relative "../lib/nybooks/scraper.rb"
+require_relative "../lib/nybooks/nybooks.rb"
+require_relative "../lib/nybooks/version.rb"
