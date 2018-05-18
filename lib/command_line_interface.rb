d7d9500a204cf21295f5949e655efcaf56af56d0
KTAmoguis <ktamoguis@gmail.com>
2018-05-18 10:22:04 +0800

    modify cli main menu code

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -3,4 +3,54 @@ require 'pry'
 
 class CommandLineInterface
 
+  BASEPATH = "https://www.nytimes.com/books/best-sellers/hardcover-fiction/"
+
+    def run
+      main_menu
+      #generate_book_list
+    end
+
+    def main_menu
+      puts "Welcome to NY Best Seller Books:"
+      puts "Please select from the menu below:"
+      puts "1. Hardcover Fiction"
+      puts "2. Hardcove NonFiction"
+      puts "3. Exit"
+      input = gets.strip
+      #binding.pry
+      case input.to_i
+      when 1
+        #generate_book_list1
+        second_menu
+      when 2
+        #generate_book_list2
+        second_menu
+      else
+      end
+    end
+
+    def second_menu
+      puts "Type 'Exit' or press '1' to return to main menu"
+      input = gets.strip
+
+      if input.to_i == 1
+        main_menu
+      elsif input != "Exit"
+        second_menu
+      end
+
+    end
+
+    def display_booklist
+      #puts "display all stocks"
+      stockhash = {:name =>"GOOG"}
+      .new(stockhash).display_all_stocks
+      second_menu
+    end
+
+
+    def generate_stock_info
+      bookarray = Scraper.scrape_page(BASEPATH)
+    end
+
 end
--- lib/nybooks.rb
+++ lib/nybooks.rb
@@ -1,5 +1,10 @@
-require "nybooks/version"
+#require "nybooks/version"
+require_relative "../lib/command_line_interface.rb"
 
 #module Nybooks
   # Your code goes here...
 #end
+
+class Books
+
+end
