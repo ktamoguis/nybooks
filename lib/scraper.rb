1c72d5ec90d1d51e2b64507f96d8eac1b3ae4f05
KTAmoguis <ktamoguis@gmail.com>
2018-05-18 13:28:39 +0800

    added scraper content

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,27 +9,22 @@ class Scraper
   def self.scrape_page(url)
     webpage = Nokogiri::HTML(open(url))
     books = []
-
-    #each book = webpage.css("ol.book-menu li")[4] <- iterate by even nos.
-    #freshness = webpage.css("ol.book-menu li")[4].css("div.book-body p.freshness").text
-    #title = webpage.css("ol.book-menu li")[4].css("div.book-body h2.title").text
-    #author = webpage.css("ol.book-menu li")[4].css("div.book-body p.author").text
-    #description = webpage.css("ol.book-menu li")[4].css("div.book-body p.description").text
-    #rank = webpage.css("ol.book-menu li")[29].css("button").attribute("data-rank").value
-
-    #author = webpage.css("ol.book-menu li")[30].css("button").attribute("data-author").value
-
+    doc = webpage.css("ol.book-menu div.book-body")
+    doc.each do |book|
+      title = doc.css("h2.title").text
+      author = doc.css("p.author").text
+      description = doc.css("p.description").text
+      freshness = doc.css("p.freshness").text
+      books << {:title => title, :author => author, :description = description, :freshness = freshness}
+    end
+    books
+    binding.pry
+  end
     #books = webpage.css("ol.book-menu div.book-body")[0]
     #title = webpage.css("ol.book-menu div.book-body")[0].css("h2.title").text
     #author = webpage.css("ol.book-menu div.book-body")[0].css("p.author").text
     #description = webpage.css("ol.book-menu div.book-body")[0].css("p.description").text
-    #rank = 
 
-
-    binding.pry
-
-
-    books
-  end
+  
 
 end
