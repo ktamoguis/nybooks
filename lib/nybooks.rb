81ef292ee6d8a5d1a24fe4dda01cf594090f121a
KTAmoguis <ktamoguis@gmail.com>
2018-05-18 13:37:08 +0800

    wrote new methods

--- lib/nybooks.rb
+++ lib/nybooks.rb
@@ -1,11 +1,33 @@
 #require "nybooks/version"
 require_relative "../lib/command_line_interface.rb"
 require_relative "../lib/scraper.rb"
-
+require 'pry'
 #module Nybooks
   # Your code goes here...
 #end
 
 class Books
+
+  attr_accessor :title, :author, :description, :freshness
+
+  @@all = []
+
+  def initialize(bookhash)
+    @title = bookhash[:title]
+    @author = bookhash[:author]
+    @description = bookhash[:description]
+    @freshness = bookhash[:freshness]
+    @@all << self
+  end
+
+  def self.create_from_collection(books_array)
+    books_array.each do |book|
+      Books.new(book)
+    end
+  end
+
+  def self.all
+    @@all
+  end
 
 end
