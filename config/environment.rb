eb6aaa9a20968340b2e33690fd0909005b1df53d
KTAmoguis <ktamoguis@gmail.com>
2018-05-19 10:33:53 +0800

    removed require nybooks in config

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'pry'
 require 'open-uri'
-require 'nybooks'
+
 
 require_relative "../lib/nybooks/command_line_interface.rb"
 require_relative "../lib/nybooks/scraper.rb"
